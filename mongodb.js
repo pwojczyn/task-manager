ee826977212c75bb817a058bf07d8ff964fba269
pwojczyn <pwojczyn@gmail.com>
2019-12-13 17:44:11 +0100

    Querying Documents

--- mongodb.js
+++ mongodb.js
@@ -1,68 +1,50 @@
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
-
-const {
-    MongoClient,
-    ObjectID
-} = require('mongodb')
-
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
+const { MongoClient, ObjectID } = require("mongodb");
 
 // local mongodb
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
 // remote mongodb mLab
 // const connectionURL = 'mongodb://test100:test100@ds353738.mlab.com:53738/heroku_nnn46r7g'
 // const databaseName = 'heroku_nnn46r7g'
 
-MongoClient.connect(connectionURL, {
+MongoClient.connect(
+  connectionURL,
+  {
     useNewUrlParser: true
-}, (error, client) => {
+  },
+  (error, client) => {
     if (error) {
-        return console.log('Unable to connect to database!')
+      return console.log("Unable to connect to database!");
     }
-    console.log('Connected corretly!')
+    console.log("Connected corretly!");
 
-    const db = client.db(databaseName)
+    const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Vikram',
-    //     age: 62
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection("users").findOne(
+      { _id: new ObjectID("5df271a167f9cc04dfb4675b") },
+      (error, user) => {
+        if (error) {
+          return console.log("Unable to fetch data");
+        }
+        console.log(user);
+      }
+    );
+    db.collection("users")
+      .find({ age: 39 })
+      .toArray((error, users) => {
+        console.log(users);
+      });
+    db.collection("tasks").findOne(
+      { _id: new ObjectID("5df2731291cf8610ae97f54e") },
+      (error, task) => {
+        // console.log(task);
+      }
+    );
 
-    // db.collection('users').insertMany([{
-    //     name: 'Jen',
-    //     age: 28
-    // }, {
-    //     name: 'Gunther',
-    //     age: 27
-    // }], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert many users!')
-    //     }
-    //     console.log(result.ops)
-    // })
-    // db.collection('tasks').insertMany([{
-    //     description: 'Buy new tv',
-    //     completed: false
-    // }, {
-    //     description: 'Buy new iPhone',
-    //     completed: false
-    // }, {
-    //     description: 'Buy new shoes',
-    //     completed: true
-    // }], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert many tasks !')
-    //     }
-    //     console.log(result.ops)
-    // })
-})
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        console.log(tasks);
+      });
+  }
+);
